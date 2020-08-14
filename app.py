bcc9df2ac608b2cc7684c2d4a70749add072a05a
Michelle CAO <jiahu0724417@gmail.com>
2020-08-14 22:49:21 +0800

    Update1

--- app.py
+++ app.py
@@ -40,10 +40,12 @@ def handle_message(event):
     msg = event.message.text
     r = '很抱歉，您說什麼'
 
-    if msg =='hi':
-        r = 'hi'
-    elif msg == '你吃飯了嗎?':
-        r = '還沒'
+    if msg in ['hi', 'Hi']:
+        r = '嗨'
+    elif msg == '你是誰':
+        r = '我是機器人'
+    elif '訂位' in msg:
+        r = '您想訂位，是嗎?'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
