66bc96a69998281c09868d8ff2b6e0cb083c9d7f
Michelle CAO <jiahu0724417@gmail.com>
2020-08-14 22:43:59 +0800

    Update new

--- app.py
+++ app.py
@@ -38,7 +38,12 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    r = '你吃飯了嗎?'
+    r = '很抱歉，您說什麼'
+
+    if msg =='hi':
+        r = 'hi'
+    elif msg == '你吃飯了嗎?':
+        r = '還沒'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
