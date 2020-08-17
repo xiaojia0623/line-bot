2f0cb066208e96a60a57c28118cd701725394a9a
Michelle CAO <jiahu0724417@gmail.com>
2020-08-17 15:27:32 +0800

    update

--- app.py
+++ app.py
@@ -57,8 +57,8 @@ def handle_message(event):
             sticker_id='14'
         )
         line_bot_api.reply_message(
-        event.reply_token,
-        sticker_message)
+            event.reply_token,
+            sticker_message)
         return
 
     if msg in ['hi', 'Hi', '你好']:
